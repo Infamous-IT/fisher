9c8e862f4e2fe27f9e93ef72893a41d23f456a86
NHlukhaniuk-Pro <mac@MacBook-Pro-baza.local>
2023-08-08 10:24:05 +0300

    Create empty app with dependencies

---
+++ src/test/java/com/nhlukhaniuk/fishingapp/FishingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhlukhaniuk.fishingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
