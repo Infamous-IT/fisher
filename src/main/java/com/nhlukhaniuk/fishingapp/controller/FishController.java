3ad81da7c19b33fb14fd8f58fe394ee69e36ab29
infamous-it <gluhanuk98@gmail.com>
2023-08-31 10:02:15 +0300

    Added controller for User & Fish model

---
+++ src/main/java/com/nhlukhaniuk/fishingapp/controller/FishController.java
@@ -0,0 +1,46 @@
+package com.nhlukhaniuk.fishingapp.controller;
+
+import com.nhlukhaniuk.fishingapp.dto.FishDTO;
+import com.nhlukhaniuk.fishingapp.service.FishServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(value = "/secured/fish")
+public class FishController {
+
+    private FishServiceImpl fishService;
+
+    @Autowired
+    public FishController(FishServiceImpl fishService) {
+        this.fishService = fishService;
+    }
+
+    @GetMapping
+    public List<FishDTO> get() {
+        return fishService.get();
+    }
+
+    @GetMapping(value = "/:id")
+    public FishDTO get(UUID id) {
+        return fishService.getById(id);
+    }
+
+    @PostMapping(value = "/create")
+    public FishDTO create(FishDTO fishDTO) {
+        return fishService.create(fishDTO);
+    }
+
+    @PostMapping(value = "/update")
+    public FishDTO update(FishDTO fishDTO) {
+        return fishService.update(fishDTO);
+    }
+
+    @DeleteMapping(value = "/:id")
+    public void delete(UUID id) {
+        fishService.delete(id);
+    }
+}
--- src/main/java/com/nhlukhaniuk/fishingapp/controller/UserController.java
+++ src/main/java/com/nhlukhaniuk/fishingapp/controller/UserController.java
@@ -1,20 +1,54 @@
 package com.nhlukhaniuk.fishingapp.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.nhlukhaniuk.fishingapp.dto.UserDTO;
+import com.nhlukhaniuk.fishingapp.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import java.security.Principal;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/secured/user")
 public class UserController {
 
+    private UserServiceImpl userService;
+
+    @Autowired
+    public UserController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+//    @GetMapping
+//    public String userAccess(Principal principal) {
+//        if (principal == null) {
+//            return null;
+//        }
+//        return principal.getName();
+//    }
+
+    @PostMapping(value = "/create")
+    public UserDTO create(UserDTO userDTO) {
+        return userService.create(userDTO);
+    }
+
     @GetMapping
-    public String userAccess(Principal principal) {
-        if (principal == null) {
-            return null;
-        }
-        return principal.getName();
+    public List<UserDTO> get() {
+        return userService.get();
+    }
+
+    @GetMapping(value = "/:id")
+    public UserDTO get(UUID id) {
+        return userService.getById(id);
+    }
+
+    @PostMapping(value = "/update")
+    public UserDTO update(UserDTO userDTO) {
+        return userService.update(userDTO);
+    }
+
+    @DeleteMapping(value = "/:id")
+    public void delete(UUID id) {
+        userService.delete(id);
     }
 }
